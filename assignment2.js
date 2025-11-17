23be4bcb2bf40319c346035a1e3edd01f6d1829e
jacob gichira mbuco <mbucojacob@gmail.com>
2025-11-17 15:03:07 +0300

    Print all even numbers form 1 to 20 using a for loop and an if condition

--- assignment2.js
+++ assignment2.js
@@ -266,5 +266,13 @@ console.log("23. Print numbers from 1 to 10:");
 for (let i = 1; i <= 10; i++) {
     console.log(i);
 }       
-console.log("--------------------------------------------------\n");        
+console.log("--------------------------------------------------\n"); 
+/* 2. Print all even numbers from 1 to 20. */
+console.log("24. Print all even numbers from 1 to 20:");
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {  
+        console.log(i);
+    }
+}
+console.log("--------------------------------------------------\n");       
 
